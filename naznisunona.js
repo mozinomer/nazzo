9aa69ca5b2a1d6c0fb12428515066e0d47febb53
mozinomer <mozinomer12@gmail.com>
2021-02-16 12:39:24 +0500

    sa

--- naznisunona.js
+++ naznisunona.js
@@ -5,3 +5,17 @@ $('.sliderContainerBlackPanther').slick({
 	dots: true,
 	centerMode: false,
 })
+$('.sliderproducts').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	arrows: false,
+	dots: false,
+	centerMode: false,
+})
+$('.rowsliderContainerBlog').slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	arrows: false,
+	dots: false,
+	centerMode: false,
+})
