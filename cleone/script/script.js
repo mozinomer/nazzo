7b79aee2e750a6882a1084f6a0bc663b34e33cdb
mozinomer <mozinomer12@gmail.com>
2021-03-12 12:43:38 +0500

    as

---
+++ cleone/script/script.js
@@ -0,0 +1,54 @@
+$('.sliderContainer').slick({
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    infinite: false,
+    centerMode: false,
+});
+$('.ImagesContainerPost').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: false,
+    centerMode: false,
+    fade: true,
+    asNavFor: '.sliderRightPostContainer'
+});
+$('.sliderRightPostContainer').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.ImagesContainerPost',
+    dots: false,
+    infinite: false,
+    centerMode: false,
+    focusOnSelect: false
+});
+
+
+$('.shopProductSlider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: '.shopProductSliderthumb',
+    arrows: false
+});
+$('.shopProductSliderthumb').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.shopProductSlider',
+    arrows: false,
+    focusOnSelect: true
+});
+$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
